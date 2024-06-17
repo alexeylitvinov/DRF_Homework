7b067b80b7ca7265b6bc55b0f879c96c9554295e
alexeylitvinov <alextwin1976@gmail.com>
2024-06-17 19:35:18 +0200

    add models Course adn Lesson

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='blogs_images', **NULLABLE, verbose_name='Превью')
+
+    class Meta:
+        db_table = 'courses'
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='blogs_images', **NULLABLE, verbose_name='Превью')
+    video_url = models.URLField(max_length=200, **NULLABLE, verbose_name='Ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    class Meta:
+        db_table = 'lessons'
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return f'{self.title} {self.course}'
